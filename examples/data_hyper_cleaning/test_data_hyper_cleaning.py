96e67de360edf7e929b41ce5737c2dfa42fc0170
QHofmann <121963021+QHofmann@users.noreply.github.com>
2025-09-11 14:09:15 +0800

    fix: fix bug of hyper_cleaning

--- examples/data_hyper_cleaning/test_data_hyper_cleaning.py
+++ examples/data_hyper_cleaning/test_data_hyper_cleaning.py
@@ -23,7 +23,7 @@ hyper_methodlist = (
     ["IGA"],
     ["IGA", "PTT"],
 )
-dynamic_method_dm = (["DM"], ["DM", "GDA, NGD"])
+dynamic_method_dm = (["DM"], ["DM", "GDA,NGD"])
 hyper_method_dm = (["RAD"], ["CG"])
 fogm_method = (["VSM"], ["VFM"], ["MESM"], ["PGDM"])
 
