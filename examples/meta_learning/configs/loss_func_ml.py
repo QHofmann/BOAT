e3686f728cada24676b9667a1e6ec11bddd809e7
QHofmann <121963021+QHofmann@users.noreply.github.com>
2025-09-11 19:08:10 +0800

    更新 loss_func_ml.py

--- examples/meta_learning/configs/loss_func_ml.py
+++ examples/meta_learning/configs/loss_func_ml.py
@@ -27,6 +27,16 @@ def maml_loss(ll_feed_dict, upper_model, lower_model, weights=0.0, **kwargs):
 def gda_loss(
     ll_feed_dict, ul_feed_dict, upper_model, lower_model, weights=0.0, **kwargs
 ):
+    out = ll_feed_dict["alpha"] * mse_loss(
+        lower_model(ul_feed_dict["data"], **kwargs), ul_feed_dict["target"]
+    ) + (1 - ll_feed_dict["alpha"]) * mse_loss(
+        lower_model(ll_feed_dict["data"], **kwargs), ll_feed_dict["target"]
+    )
+    return out
+
+def gda_loss_meha(
+    ll_feed_dict, ul_feed_dict, upper_model, lower_model, weights=0.0, **kwargs
+):
     out = ll_feed_dict["alpha"] * F.cross_entropy(
         lower_model(upper_model(ul_feed_dict["data"]), **kwargs), ul_feed_dict["target"].long()
     ) + (1 - ll_feed_dict["alpha"]) * F.cross_entropy(
