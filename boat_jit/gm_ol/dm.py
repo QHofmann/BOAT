4e5b8fc7cfe4e4f1a1f50e37938bbd45a7b4baad
QHofmann <121963021+QHofmann@users.noreply.github.com>
2025-12-29 14:18:18 +0800

    22

--- boat_jit/gm_ol/dm.py
+++ boat_jit/gm_ol/dm.py
@@ -1,20 +1,17 @@
-from .dynamical_system import DynamicalSystem
-import jittor as jit
-from jittor import Module
-from ..higher_jit.patch import _MonkeyPatchBase
-from ..higher_jit.optim import DifferentiableOptimizer
+import torch
+from torch.nn import Module
+from higher.patch import _MonkeyPatchBase
+from higher.optim import DifferentiableOptimizer
 from typing import Dict, Any, Callable
-from ..utils.op_utils import (
+from boat_torch.utils.op_utils import (
     update_tensor_grads,
     grad_unused_zero,
     list_tensor_norm,
     list_tensor_matmul,
-    custom_grad,
-    manual_update,
 )
 
-from boat_jit.operation_registry import register_class
-from boat_jit.gm_ol.dynamical_system import DynamicalSystem
+from boat_torch.operation_registry import register_class
+from boat_torch.gm_ol.dynamical_system import DynamicalSystem
 
 
 @register_class
@@ -28,42 +25,46 @@ class DM(DynamicalSystem):
         The lower-level objective function of the BLO problem.
     ul_objective : Callable
         The upper-level objective function of the BLO problem.
-    ll_model : jittor.Module
+    ll_model : torch.nn.Module
         The lower-level model of the BLO problem.
-    ul_model : jittor.Module
+    ul_model : torch.nn.Module
         The upper-level model of the BLO problem.
     lower_loop : int
         The number of iterations for the lower-level optimization process.
     solver_config : Dict[str, Any]
-        A dictionary containing configurations for the optimization solver, including
-        hyperparameters and specific settings for NGD, GDA, and DM.
+        A dictionary containing configurations for the optimization solver, including hyperparameters and specific settings for NGD, GDA, and DM.
 
     References
     ----------
-    [1] Liu R, Liu Y, Yao W, et al., "Averaged method of multipliers for bi-level optimization without lower-level
-        strong convexity," ICML, 2023.
+    [1] Liu R, Liu Y, Yao W, et al., "Averaged method of multipliers for bi-level optimization without lower-level strong convexity," ICML, 2023.
     """
 
     def __init__(
-        self,
-        ll_objective: Callable,
-        lower_loop: int,
-        ul_model: Module,
-        ul_objective: Callable,
-        ll_model: Module,
-        solver_config: Dict[str, Any],
+            self,
+            ll_objective: Callable,
+            lower_loop: int,
+            ul_model: Module,
+            ul_objective: Callable,
+            ll_model: Module,
+            solver_config: Dict[str, Any],
     ):
+
         super(DM, self).__init__(
             ll_objective, ul_objective, lower_loop, ul_model, ll_model, solver_config
         )
+        self.solver_config["copy_last_param"] = False
         self.truncate_max_loss_iter = "PTT" in solver_config["na_op"]
         self.alpha = solver_config["GDA"]["alpha_init"]
         self.alpha_decay = solver_config["GDA"]["alpha_decay"]
-        self.truncate_iters = solver_config["RGT"]["truncate_iter"]
+        self.truncate_iters = solver_config["RGT"]["truncate_iter"] if "RGT" in solver_config["na_op"] else 0
         self.ll_opt = solver_config["lower_level_opt"]
         self.ul_opt = solver_config["upper_level_opt"]
-        self.auxiliary_v = solver_config["DM"]["auxiliary_v"]
-        self.auxiliary_v_opt = solver_config["DM"]["auxiliary_v_opt"]
+        self.auxiliary_v = [
+            torch.zeros_like(param) for param in list(solver_config["lower_level_var"])
+        ]
+        self.auxiliary_v_opt = torch.optim.SGD(
+            self.auxiliary_v, lr=solver_config["DM"]["auxiliary_v_lr"]
+        )
         self.auxiliary_v_lr = solver_config["DM"]["auxiliary_v_lr"]
         self.tau = solver_config["DM"]["tau"]
         self.p = solver_config["DM"]["p"]
@@ -74,14 +75,14 @@ class DM(DynamicalSystem):
         self.gda_loss = solver_config.get("gda_loss", None)
 
     def optimize(
-        self,
-        ll_feed_dict: Dict,
-        ul_feed_dict: Dict,
-        auxiliary_model: _MonkeyPatchBase,
-        auxiliary_opt: DifferentiableOptimizer,
-        current_iter: int,
-        next_operation: str = None,
-        **kwargs
+            self,
+            ll_feed_dict: Dict,
+            ul_feed_dict: Dict,
+            auxiliary_model: _MonkeyPatchBase,
+            auxiliary_opt: DifferentiableOptimizer,
+            current_iter: int,
+            next_operation: str = None,
+            **kwargs
     ):
         """
         Executes the lower-level optimization procedure with support for NGD, GDA, and RAD operations.
@@ -122,8 +123,14 @@ class DM(DynamicalSystem):
         ------
         AssertionError
             If `next_operation` is not `None` for NGD or if an unsupported strategy is specified for GDA.
+
+        References
+        ----------
+        [1] Liu R, Liu Y, Yao W, et al. "Averaged method of multipliers for bi-level optimization without lower-level strong convexity", in ICML, 2023.
         """
-        assert next_operation is None, "NGD does not support next_operation"
+
+
+        assert self.lower_loop == 1, "DM only supports one-step lower-level optimization."
         if "gda_loss" in kwargs:
             gda_loss = kwargs["gda_loss"]
             assert self.strategy in [
@@ -134,59 +141,65 @@ class DM(DynamicalSystem):
             if self.strategy == "s1":
                 self.alpha = self.mu0 * 1 / (current_iter + 1) ** (1 / self.p)
                 self.eta = (
-                    (current_iter + 1) ** (-0.5 * self.tau)
-                    * self.alpha**2
-                    * self.ll_opt.defaults["lr"]
+                        (current_iter + 1) ** (-0.5 * self.tau)
+                        * self.alpha ** 2
+                        * self.ll_opt.defaults["lr"]
                 )
                 x_lr = (
-                    (current_iter + 1) ** (-1.5 * self.tau)
-                    * self.alpha**7
-                    * self.ll_opt.defaults["lr"]
+                        (current_iter + 1) ** (-1.5 * self.tau)
+                        * self.alpha ** 7
+                        * self.ll_opt.defaults["lr"]
                 )
             elif self.strategy == "s2":
                 self.alpha = self.mu0 * 1 / (current_iter + 1) ** (1 / self.p)
                 self.eta = (
-                    (current_iter + 1) ** (-0.5 * self.tau)
-                    * self.alpha
-                    * self.ll_opt.defaults["lr"]
+                        (current_iter + 1) ** (-0.5 * self.tau)
+                        * self.alpha
+                        * self.ll_opt.defaults["lr"]
                 )
                 x_lr = (
-                    (current_iter + 1) ** (-1.5 * self.tau)
-                    * self.alpha**5
-                    * self.ll_opt.defaults["lr"]
+                        (current_iter + 1) ** (-1.5 * self.tau)
+                        * self.alpha ** 5
+                        * self.ll_opt.defaults["lr"]
                 )
             elif self.strategy == "s3":
                 self.alpha = self.mu0 * 1 / (current_iter + 1) ** (1 / self.p)
                 self.eta = (current_iter + 1) ** (
-                    -0.5 * self.tau
+                        -0.5 * self.tau
                 ) * self.ll_opt.defaults["lr"]
                 x_lr = (
-                    (current_iter + 1) ** (-1.5 * self.tau)
-                    * self.alpha**3
-                    * self.ll_opt.defaults["lr"]
+                        (current_iter + 1) ** (-1.5 * self.tau)
+                        * self.alpha ** 3
+                        * self.ll_opt.defaults["lr"]
                 )
             for params in self.ul_opt.param_groups:
                 params["lr"] = x_lr
+
         else:
             gda_loss = None
-            assert (
-                self.strategy == "s1"
-            ), "Only 's1' strategy is supported for DM without GDA operation."
+            if "RAD" in self.na_op:
+                assert (
+                        self.strategy == "s1"
+                ), "Only 's1' strategy is supported for DM without GDA operation."
 
-            x_lr = (
-                self.ul_opt.defaults["lr"]
-                * (current_iter + 1) ** (-self.tau)
-                * self.ll_opt.defaults["lr"]
-            )
-            eta = (
-                self.eta
-                * (current_iter + 1) ** (-0.5 * self.tau)
-                * self.ll_opt.defaults["lr"]
-            )
-            for params in self.auxiliary_v_opt.param_groups:
-                params["lr"] = eta
-            for params in self.ul_opt.param_groups:
-                params["lr"] = x_lr
+                x_lr = (
+                        self.ul_opt.defaults["lr"]
+                        * (current_iter + 1) ** (-self.tau)
+                        * self.ll_opt.defaults["lr"]
+                )
+                eta = (
+                        self.eta
+                        * (current_iter + 1) ** (-0.5 * self.tau)
+                        * self.ll_opt.defaults["lr"]
+                )
+                for params in self.auxiliary_v_opt.param_groups:
+                    params["lr"] = eta
+                for params in self.ul_opt.param_groups:
+                    params["lr"] = x_lr
+
+        self.ll_opt.zero_grad()
+        self.auxiliary_v_opt.zero_grad()
+        upper_loss = self.ul_objective(ul_feed_dict, self.ul_model, auxiliary_model)
 
         if gda_loss is not None:
             ll_feed_dict["alpha"] = self.alpha
@@ -195,21 +208,23 @@ class DM(DynamicalSystem):
             )
         else:
             loss_full = self.ll_objective(ll_feed_dict, self.ul_model, auxiliary_model)
-
-        grad_y_temp = jit.grad(
-            loss_full, list(auxiliary_model.parameters()), retain_graph=True
+        grad_y_temp = torch.autograd.grad(
+            loss_full, auxiliary_model.parameters(), retain_graph=True
         )
-        upper_loss = self.ul_objective(ul_feed_dict, self.ul_model, auxiliary_model)
+
         grad_outer_params = grad_unused_zero(
             upper_loss, list(auxiliary_model.parameters()), retain_graph=True
         )
         grads_phi_params = grad_unused_zero(
-            loss_full, list(auxiliary_model.parameters()), retain_graph=True
+            loss_full,
+            list(auxiliary_model.parameters()),
+            create_graph=True,
+            retain_graph=True,
         )
-        grads = custom_grad(
+        grads = grad_unused_zero(
             grads_phi_params,
             list(self.ul_model.parameters()),
-            self.auxiliary_v,
+            grad_outputs=self.auxiliary_v,
             retain_graph=True,
         )  # dx (dy f) v
         grad_outer_hparams = grad_unused_zero(
@@ -217,19 +232,17 @@ class DM(DynamicalSystem):
         )
 
         if "RAD" in self.na_op:
-            vsp = custom_grad(
+            vsp = grad_unused_zero(
                 grads_phi_params,
                 list(auxiliary_model.parameters()),
                 grad_outputs=self.auxiliary_v,
             )  # dy (dy f) v=d2y f v
 
             for v0, v, gow in zip(self.auxiliary_v, vsp, grad_outer_params):
-                v0._custom_grad = v - gow
+                v0.grad = v - gow
             update_tensor_grads(list(self.ll_model.parameters()), grad_y_temp)
-
-            manual_update(self.ll_opt, list(self.ll_model.parameters()))
-
-            manual_update(self.auxiliary_v_opt, self.auxiliary_v)
+            self.ll_opt.step()
+            self.auxiliary_v_opt.step()
 
             grads = [
                 -g + v if g is not None else v
@@ -238,38 +251,35 @@ class DM(DynamicalSystem):
             update_tensor_grads(list(self.ul_model.parameters()), grads)
 
         else:
-            vsp = custom_grad(
+
+            vsp = torch.autograd.grad(
                 grads_phi_params,
                 list(auxiliary_model.parameters()),
                 grad_outputs=self.auxiliary_v,
-            )
-
+                retain_graph=True,
+                allow_unused=True,
+            )  # dy (dy f) v=d2y f v
             tem = [v - gow for v, gow in zip(vsp, grad_outer_params)]
 
             ita_u = list_tensor_norm(tem) ** 2
-            grad_tem = custom_grad(
-                grads_phi_params, list(auxiliary_model.parameters()), grad_outputs=tem
-            )
+            grad_tem = torch.autograd.grad(
+                grads_phi_params,
+                list(auxiliary_model.parameters()),
+                grad_outputs=tem,
+                retain_graph=True,
+                allow_unused=True,
+            )  # dy (dy f) v=d2y f v
+
             ita_l = list_tensor_matmul(tem, grad_tem)
 
             ita = ita_u / (ita_l + 1e-12)
-
             self.auxiliary_v = [
                 v0 - ita * v + ita * gow
                 for v0, v, gow in zip(self.auxiliary_v, vsp, grad_outer_params)
-            ]
-
-            vsp = custom_grad(
-                grads_phi_params,
-                list(auxiliary_model.parameters()),
-                grad_outputs=self.auxiliary_v,
-            )
-
-            for v0, v, gow in zip(self.auxiliary_v, vsp, grad_outer_params):
-                v0._custom_grad = v - gow
+            ]  # (I-ita*d2yf)v+ita*dy F)
 
             update_tensor_grads(list(self.ll_model.parameters()), grad_y_temp)
-            manual_update(self.ll_opt, list(self.ll_model.parameters()))
+            self.ll_opt.step()
 
             grads = [
                 -g + v if g is not None else v
@@ -277,4 +287,4 @@ class DM(DynamicalSystem):
             ]
             update_tensor_grads(list(self.ul_model.parameters()), grads)
 
-        return -1
+        return upper_loss.item(), -1
